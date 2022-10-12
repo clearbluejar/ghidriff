7fdcaaad69ad0c25f60225ca48411808a7ca32eb
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2022-10-12 03:58:54 +0000

    convert to python package

---
+++ ghidra_diff_engine/__main__.py
@@ -0,0 +1,10 @@
+def main():
+    """
+    GhidraDiffEngine module main function
+    """    
+
+    print("Nothing to do here yet. Go build your first Diffing Engine.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup()
