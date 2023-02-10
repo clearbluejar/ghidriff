763c0e69990f86b7ff6240fa497d382c1fb995c1
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2023-02-10 14:04:12 +0000

    fix cyclical import bug

--- ghidra_diff_engine/__init__.py
+++ ghidra_diff_engine/__init__.py
@@ -3,10 +3,7 @@ __author__ = 'clearbluejar'
 
 # Expose API
 from .ghidra_diff_engine import GhidraDiffEngine
-from .simple_diff import GhidraSimpleDiff
-from .structural_graph_diff import GhidraStructualGraphDiff
-from .version_tracking_diff import VersionTrackingDiff
 
 __all__ = [
-    "GhidraDiffEngine", "GhidraSimpleDiff", "GhidraStructualGraphDiff", "VersionTrackingDiff"
+    "GhidraDiffEngine"
 ]
