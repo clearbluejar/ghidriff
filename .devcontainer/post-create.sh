5ab68125937ea232ec25f49feebda70b298f1f4a
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2023-07-19 04:51:49 +0000

    update devcontainer to ghidra-python

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,6 +2,9 @@
 python3 -m venv .env
 source .env/bin/activate
 
+# upgrade pip
+pip install --upgrade pip
+
 # Download latest pyi typings for Ghidra Version
 pip install ghidra-stubs
 
@@ -12,3 +15,18 @@ pip install pyhidra
 if uname -a | grep -q 'aarch64'; then
     $GHIDRA_INSTALL_DIR/support/buildNatives
 fi
+
+# install local workspace
+pip install -e .
+
+# Setup Ghidra Dev for Reference
+# git clone https://github.com/NationalSecurityAgency/ghidra.git ~/ghidra-master
+# pushd ~/ghidra-master
+
+# # Follow setup from https://github.com/NationalSecurityAgency/ghidra/blob/master/DevGuide.md
+# gradle -I gradle/support/fetchDependencies.gradle init
+# gradle prepdev
+
+# popd
+
+# echo 'To open up a Ghidra latest dev: code ~/ghidra-master'
