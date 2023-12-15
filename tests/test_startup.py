b461905142be5aabb4d02b747d2a855dd8dce2c3
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2023-12-15 16:24:51 +0000

    Add test for decomp correlate #59, and also update classifies for #53

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -1,23 +1,27 @@
-import pyhidra
+from ghidriff import GhidraDiffEngine
+# from mock import patch
+from pytest import MonkeyPatch
+import pytest
+
 
 def test_pyhidra_start():
+    import pyhidra
     pyhidra.start(verbose=True)
+
 
 def test_ghidra_install_dir():
     import os
     install_dir = os.getenv("GHIDRA_INSTALL_DIR")
     assert install_dir == "/ghidra"
 
-#from ghidriff import GhidraDiffEngine
-# from pytest import MonkeyPatch
 
 # @pytest.fixture
 # def setup_bogus_env(mon):
-# from mock import patch
+
 
 # @patch('pyhidra.GHIDRA_INSTALL_DIR', "/someboguspath"):
 # def test_bogus_ghidra_install_dira():
-#     with pytest.raises(FileNotFoundError):            
+#     with pytest.raises(FileNotFoundError):
 #         import pyhidra as err_pyhidra
 #         err_pyhidra.start(verbose=True)
 
@@ -27,8 +31,8 @@ def test_ghidra_install_dir():
 #     # sys.modules.pop('pyhidra')
 #     with MonkeyPatch.context() as mp:
 #         mp.delenv("GHIDRA_INSTALL_DIR")
-        
-#         import pyhidra    
+
+#         import pyhidra
 #         # print(os.getenv("GHIDRA_INSTALL_DIR"))
 #         with pytest.raises(SystemExit) as pytest_wrapped_e:
 #             launcher = pyhidra.start(verbose=True)
@@ -40,24 +44,29 @@ def test_ghidra_install_dir():
 #     # reload(pyhidra)
 #     import sys
 #     del sys.modules['pyhidra']
-        
 
 
+# @pytest.mark.forked
 # def test_bogus_ghidra_install_dir():
-#     #monkeypatch.setenv("GHIDRA_INSTALL_DIR", '/someboguspath')        
+#     # monkeypatch.setenv("GHIDRA_INSTALL_DIR", '/someboguspath')
 #     import os
-#     print(os.getenv("GHIDRA_INSTALL_DIR"))
-#     with MonkeyPatch.context() as mp:        
-#         with pytest.raises(FileNotFoundError):                                    
-#             import pyhidra
-#             #mp.setattr(pyhidra.constants,'GHIDRA_INSTALL_DIR', '/someboguspath')            
-#             pyhidra.start(verbose=True)
 
-        
+#     with MonkeyPatch.context() as mp:
+#         mp.delenv('GHIDRA_INSTALL_DIR')
+#         print(os.getenv("GHIDRA_INSTALL_DIR"))
 
+#         launcher = None
 
-    
+#         # with pytest.raises(SystemExit):
+#         import pyhidra
+#         mp.setattr(pyhidra.constants, 'GHIDRA_INSTALL_DIR', '/someboguspath')
+#         print(pyhidra.constants)
+#         print(os.getenv("GHIDRA_INSTALL_DIR"))
+#         launcher = pyhidra.start(verbose=True)
+
+#         assert launcher == None
+
     #     launcher = pyhidra.start(verbose=True)
     #     print(launcher.check_ghidra_version())
 
-#det test_file_not_exist():
+# det test_file_not_exist():
