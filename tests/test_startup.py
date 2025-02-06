4d710a9cdfae941678c4966901f41a769ab84391
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2025-02-06 20:24:52 +0000

    pyhidra -> pyghidra

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -4,9 +4,9 @@ from pytest import MonkeyPatch
 import pytest
 
 
-def test_pyhidra_start():
-    import pyhidra
-    pyhidra.start(verbose=True)
+def test_pyghidra_start():
+    import pyghidra
+    pyghidra.start(verbose=True)
 
 
 def test_ghidra_install_dir():
@@ -19,31 +19,31 @@ def test_ghidra_install_dir():
 # def setup_bogus_env(mon):
 
 
-# @patch('pyhidra.GHIDRA_INSTALL_DIR', "/someboguspath"):
+# @patch('pyghidra.GHIDRA_INSTALL_DIR', "/someboguspath"):
 # def test_bogus_ghidra_install_dira():
 #     with pytest.raises(FileNotFoundError):
-#         import pyhidra as err_pyhidra
-#         err_pyhidra.start(verbose=True)
+#         import pyghidra as err_pyghidra
+#         err_pyghidra.start(verbose=True)
 
 
 # def test_ghidra_install_dir():
 #     # import sys
-#     # sys.modules.pop('pyhidra')
+#     # sys.modules.pop('pyghidra')
 #     with MonkeyPatch.context() as mp:
 #         mp.delenv("GHIDRA_INSTALL_DIR")
 
-#         import pyhidra
+#         import pyghidra
 #         # print(os.getenv("GHIDRA_INSTALL_DIR"))
 #         with pytest.raises(SystemExit) as pytest_wrapped_e:
-#             launcher = pyhidra.start(verbose=True)
+#             launcher = pyghidra.start(verbose=True)
 #         assert pytest_wrapped_e.type == SystemExit
 #         #assert pytest_wrapped_e.value.code == 42
 #     import os
 #     print(os.getenv("GHIDRA_INSTALL_DIR"))
 #     # from importlib import reload
-#     # reload(pyhidra)
+#     # reload(pyghidra)
 #     import sys
-#     del sys.modules['pyhidra']
+#     del sys.modules['pyghidra']
 
 
 # @pytest.mark.forked
@@ -58,15 +58,15 @@ def test_ghidra_install_dir():
 #         launcher = None
 
 #         # with pytest.raises(SystemExit):
-#         import pyhidra
-#         mp.setattr(pyhidra.constants, 'GHIDRA_INSTALL_DIR', '/someboguspath')
-#         print(pyhidra.constants)
+#         import pyghidra
+#         mp.setattr(pyghidra.constants, 'GHIDRA_INSTALL_DIR', '/someboguspath')
+#         print(pyghidra.constants)
 #         print(os.getenv("GHIDRA_INSTALL_DIR"))
-#         launcher = pyhidra.start(verbose=True)
+#         launcher = pyghidra.start(verbose=True)
 
 #         assert launcher == None
 
-    #     launcher = pyhidra.start(verbose=True)
+    #     launcher = pyghidra.start(verbose=True)
     #     print(launcher.check_ghidra_version())
 
 # det test_file_not_exist():
